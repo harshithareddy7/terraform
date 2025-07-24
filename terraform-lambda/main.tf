c86d40103e76da090660cbce2fa4217b1af7a810
harshithareddy7 <harshithareddy900@gmail.com>
2025-07-24 09:05:37 -0400

    A simple Lambda function with execution role

---
+++ terraform-lambda/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_iam_policy_document" "assume_role"{
+    statement{
+        effect="Allow"
+
+        principals{
+            type="Service"
+            identifiers=["lambda.amazonaws.com"]
+        }
+        actions=["sts:AssumeRole"]
+
+    }
+}
+
+resource "aws_iam_role" "lambda_execution_role"{
+    name="lambda_exec_role"
+    assume_role_policy=data.aws_iam_policy_document.assume_role.json
+
+}
+
+#Package the Lambda function code
+
+data "archive_file" "lambda_zip"{
+
+    type="zip"
+    source_file="${path.module}/lambda/index.py"
+    output_path="${path.module}/index/index.zip"
+}
+
+#Lambda function
+
+resource "aws_lambda_function" "first_lambda"{
+    filename= data.archive_file.lambda_zip.output_path
+    function_name="example_first_lambda"
+    role=aws_iam_role.lambda_execution_role.arn
+    handler="index.lambda_handler"
+    source_code_hash=data.archive_file.lambda_zip.output_base64sha256
+
+    runtime="python3.13"
+
+    environment{
+        variables={
+            ENVIRONMENT="production"
+        }
+    }
+
+}
